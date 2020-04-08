3062afd0f49d68de63e112cb414dfffed024f991
Ronan Zverik <ZverikRS@gmail.com>
2020-04-09 01:23:30 +0300

    add Hock withSuspense to App.js withSuspense(LoginContainer) withSuspense(DialogsContainer)

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import HeaderContainer from "./components/Header/HeaderContainer";
 import {connect, Provider} from "react-redux";
 import {compose} from "redux";
 import {initializedApp} from "./redux/AppReducer";
+import {withSuspense} from "./hoc/withSuspense";
 
 const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"));
 const LoginContainer = React.lazy(() => import("./components/Login/LoginContainer"));
@@ -30,24 +31,9 @@ class App extends React.Component {
                         <NavBar/>
                         <div className='app-wrapper-content'>
                             <Route exact path='/' render={() => <IndexContainer/>}/>
-                            <Route path='/login'
-                                   render={
-                                       () => {
-                                           return <React.Suspense fallback={<Preloader/>}>
-                                               <LoginContainer/>
-                                           </React.Suspense>
-                                       }
-                                   }/>
-
+                            <Route path='/login' render={withSuspense(LoginContainer)}/>
                             <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-                            <Route path='/dialogs'
-                                   render={
-                                       () => {
-                                           return <React.Suspense fallback={<Preloader/>}>
-                                               <DialogsContainer/>
-                                           </React.Suspense>
-                                       }
-                                   }/>
+                            <Route path='/dialogs' render={withSuspense(DialogsContainer)}/>
                             <Route path='/users' render={() => <UsersContainer/>}/>
                         </div>
                     </>
