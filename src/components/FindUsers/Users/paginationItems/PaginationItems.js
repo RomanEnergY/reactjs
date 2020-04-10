f6af0d4871beb1568704b54e9258ce5c3f174e39
Ronan Zverik <ZverikRS@gmail.com>
2020-04-10 23:24:52 +0300

    fixing bugs of work 'pagination' in UsersContainer #3

--- src/components/FindUsers/Users/paginationItems/PaginationItems.js
+++ src/components/FindUsers/Users/paginationItems/PaginationItems.js
@@ -3,14 +3,18 @@ import React, {useState} from "react";
 import cn from "classnames";
 
 const PaginationItems = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
-    let [portionNumber, setPortionNumber] = useState(Math.floor(currentPage / portionSize) + 1);
+    let [portionNumber, setPortionNumber] = useState(currentPage % portionSize === 0
+        ? currentPage / portionSize
+        : Math.floor(currentPage / portionSize) + 1
+    );
+
     let pagesCount = Math.ceil(totalItemsCount / pageSize);
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
     let portionCount = Math.ceil(pagesCount / portionSize);
-    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 0.9;
     let rightPortionPageNumber = portionNumber * portionSize;
 
     const clickPortionNumber = (iterator) => {
