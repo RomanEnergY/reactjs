797e6598ffd292c08b0d452e70c47b0c2c4b4b07
Ronan Zverik <ZverikRS@gmail.com>
2020-04-10 23:01:17 +0300

    fixing bugs of work 'pagination' in UsersContainer #2

--- src/components/FindUsers/Users/paginationItems/PaginationItems.js
+++ src/components/FindUsers/Users/paginationItems/PaginationItems.js
@@ -2,17 +2,8 @@ import styles from "./PaginationItems.module.css";
 import React, {useState} from "react";
 import cn from "classnames";
 
-const PaginationItems = (
-    {
-        totalItemsCount,
-        pageSize,
-        currentPage,
-        onPageChanged,
-        portionSize = 10
-    }) => {
-    let [portionNumber, setPortionNumber] = useState(1);
-    // debugger
-
+const PaginationItems = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
+    let [portionNumber, setPortionNumber] = useState(Math.floor(currentPage / portionSize) + 1);
     let pagesCount = Math.ceil(totalItemsCount / pageSize);
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
@@ -20,31 +11,25 @@ const PaginationItems = (
     }
     let portionCount = Math.ceil(pagesCount / portionSize);
     let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
-
-
     let rightPortionPageNumber = portionNumber * portionSize;
 
-    const clickPrev = () => {
-        setPortionNumber(portionNumber - 1)
+    const clickPortionNumber = (iterator) => {
+        setPortionNumber(portionNumber + iterator)
     };
 
     return <div className={styles.paginator}>
-        {portionNumber > 1 &&
-        <button onClick={clickPrev}>PREV</button>}
+        {portionNumber > 1 && <button onClick={() => clickPortionNumber(-1)}>PREV</button>}
 
         {pages
             .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
             .map((p) => {
                 return <span className={cn({[styles.selectedPage]: currentPage === p}, styles.pageNumber)}
-                             key={p}
-                             onClick={(e) => {
-                                 onPageChanged(p);
-                             }}>{p}</span>
+                             key={p} onClick={(e) => {
+                    onPageChanged(p);
+                }}>{p}</span>
             })}
-        {portionCount > portionNumber &&
-        <button onClick={() => {
-            setPortionNumber(portionNumber + 1)
-        }}>NEXT</button>}
+
+        {portionCount > portionNumber && <button onClick={() => clickPortionNumber(1)}>NEXT</button>}
     </div>
 };
 
